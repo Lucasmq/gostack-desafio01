e15f4d00d9b8585ab2913de963a69ac1c389b316
Lucas <lucasmedeirosq@gmail.com>
2020-04-14 23:51:55 -0300

    Terminando o desafio 01

--- src/app.js
+++ src/app.js
@@ -1,33 +1,90 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function validarUuid(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({error : 'Invalid repository ID.'})
+  }
+
+  return next();
+}
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repositore = { id: uuid(), title, url, techs, likes:0 }
+
+  repositories.push(repositore);
+
+  return response.json(repositore);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validarUuid, (request, response,) => {
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+    if(repositoreIndex < 0) {
+      return response.status(400).json({error: 'Respository not found.'})
+    }
+    
+    const repositore = repositories[repositoreIndex];
+
+    repositories[repositoreIndex] = {...repositore, title, url, techs};
+
+    return response.json(repositore);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validarUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+  if(repositoreIndex < 0) {
+    return response.status(400).json({error: 'Respository not found.'})
+  }
+
+  repositories.splice(repositoreIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validarUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+  console.log(repositoreIndex);
+
+  if(repositoreIndex < 0) {
+    return response.status(400).json({error: 'Respository not found.'})
+  }
+
+  let repositore = repositories[repositoreIndex];
+
+  let { likes } = repositore;
+
+  likes = likes+1;
+
+  repositore = {...repositore, likes}
+
+  repositories[repositoreIndex] = {...repositore};
+
+  return response.json(repositore);
 });
 
 module.exports = app;
